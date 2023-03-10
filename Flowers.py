ec24b013dbb36c27c230cdf5dfe8be56e3df16f7
Gerald <geraldwainaina25@gmail.com>
2023-03-10 21:07:38 +0300

    Update Flowers.py

--- Flowers.py
+++ Flowers.py
@@ -7,10 +7,11 @@ import tensorflow_datasets as tfds
 from tensorflow.keras import layers
 
 import logging
+
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
-#download dataset
+# download dataset
 (training_set, validation_set), dataset_info = tfds.load(
     'tf_flowers',  # name of dataset to download
     split=['train[:70%]', 'train[70%:]'],
@@ -20,8 +21,8 @@ logger.setLevel(logging.ERROR)
 
 num_classes = dataset_info.features['label'].num_classes
 
-num_training_examples=0
-num_validation_examples=0
+num_training_examples = 0
+num_validation_examples = 0
 
 for example in training_set:
     num_training_examples += 1
@@ -33,3 +34,19 @@ print('Total Number of Classes: {}'.format(num_classes))
 print('Total Number of Training Images: {}'.format(num_training_examples))
 print('Total Number of Validation Images: {} \n'.format(num_validation_examples))
 
+for i, example in enumerate(training_set.take(5)):
+    print('Image {} shape: {} label: {}'.format(i + 1, example[0].shape, example[1]))
+
+IMAGE_RES = 224
+
+
+def format_image(image, label):
+    image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES)) / 255.0
+    return image, label
+
+
+BATCH_SIZE = 32
+
+train_batches = training_set.shuffle(num_training_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
