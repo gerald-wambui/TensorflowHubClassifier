2696b599ec4527e7bec511f165aae749b28406c6
Gerald <geraldwainaina25@gmail.com>
2023-03-10 09:54:52 +0300

    Create Flowers.py

---
+++ Flowers.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+from tensorflow.keras import layers
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+#download datasets
+splits = ...
